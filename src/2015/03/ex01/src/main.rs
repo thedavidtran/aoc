ae06a2b5dc3290378e14430fc50271999c6b2551
David Tran <david@davidtran.io>
2023-03-12 15:53:45 -0400

    Day 3 - Solution

--- src/2015/03/ex01/src/main.rs
+++ src/2015/03/ex01/src/main.rs
@@ -1,3 +1,49 @@
+use std::collections::HashSet;
+use std::fs;
+
+#[derive(PartialEq, Eq, Hash, Clone)]
+struct Coordinate<> {
+    x: i32,
+    y: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("--- Day 3: Perfectly Spherical Houses in a Vacuum ---");
+    const FILE_PATH : &str = "./input.txt";
+    // Read input.txt
+    let input = fs::read_to_string(FILE_PATH)
+        .expect("Should have been able to read the file");
+    println!("Input from '{FILE_PATH}':\n{input}");
+
+    let mut coordinates = Coordinate { x: 0, y: 0 };
+    let mut visited: HashSet<Coordinate> = HashSet::new();
+    println!("santa started at ({}, {})", coordinates.x, coordinates.y);
+    visited.insert(coordinates.clone()); // insert initial starting spot
+    for c in input.chars() {
+        match c {
+            '<' => { // left
+                println!("{c} left");
+                coordinates.x -= 1;
+            }
+            '>' => { // right
+                println!("{c} right");
+                coordinates.x += 1;
+            }
+            '^' => { // up
+                println!("{c} up");
+                coordinates.y += 1;
+            }
+            'v' => { // down
+                println!("{c} down");
+                coordinates.y -= 1;
+            }
+            _ => todo!()
+        }
+        println!("santa is at ({}, {})", coordinates.x, coordinates.y);
+        visited.insert(coordinates.clone());
+    }
+    for history in visited.iter() {
+        println!("{}, {}", history.x, history.y);
+    }
+    println!("Santa visited {} unique houses", visited.len()); // 2081
 }
