a24ca48dceb244d2e8bf4c842d1dc3eac2b37690
David Tran <david@davidtran.io>
2023-03-12 09:26:32 -0400

    Solution using a counter

--- src/2015/01/ex01/src/main.rs
+++ src/2015/01/ex01/src/main.rs
@@ -1,3 +1,23 @@
+use std::fs;
+
 fn main() {
     println!("--- Day 1: Not Quite Lisp ---");
+
+    // Approach #1: Use a counter, increment count if '(' and decrement otherwise.
+
+    const FILE_PATH : &str = "./input.txt";
+    // Read input.txt
+    let input = fs::read_to_string(FILE_PATH)
+        .expect("Should have been able to read the file");
+    println!("Input from '{FILE_PATH}':\n{input}");
+
+    let mut count : i32 = 0;
+    for c in input.chars() {
+        if c == '(' {
+            count += 1;
+        } else {
+            count -= 1;
+        }
+    }
+    println!("Santa is on floor {count}"); // 232
 }
