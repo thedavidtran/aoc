1a45849c1e583f22da7defddf53752d03093bd64
David Tran <david@davidtran.io>
2023-03-14 22:00:59 -0400

    Day 4 - Part 2 Solution

--- src/2015/04/ex02/src/main.rs
+++ src/2015/04/ex02/src/main.rs
@@ -1,3 +1,22 @@
+extern crate crypto;
+
+use crypto::md5::Md5;
+use crypto::digest::Digest;
+
 fn main() {
-    println!("Hello, world!");
+    let mut hasher = Md5::new();
+    let key = "ckczppom";
+    let mut num = 0;
+    let target_prefix = "000000";
+    loop {
+        let input = key.to_owned() + &num.to_string();
+        hasher.input_str(&input);
+        let hash = hasher.result_str();
+        if &hash[0..target_prefix.len()] == target_prefix {
+            break;
+        }
+        num += 1;
+        hasher.reset();
+    }
+    println!("{} found with num {}", target_prefix, num); // 3938038
 }
