91f39872d3d1bb0b8625ebeb6659b2b0ab6bc202
David Tran <david@davidtran.io>
2023-03-19 12:20:34 -0400

    Day 13 - Init

---
+++ src/2015/13/ex01/src/main.rs
@@ -0,0 +1,19 @@
+use std::io::{self, Read};
+use std::fs::File;
+
+fn filename_to_string(s: &str) -> io::Result<String> {
+    let mut file = File::open(s)?;
+    let mut s = String::new();
+    file.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn main() {
+    println!("--- Day 13: Knights of the Dinner Table ---");
+    const FILE_PATH : &str = "./input.txt";
+    // Read input.txt
+    let input = filename_to_string(FILE_PATH);
+    input.unwrap().split("\n").for_each(|line| {
+        println!("{line}");
+    });
+}
